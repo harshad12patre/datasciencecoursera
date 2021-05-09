ed92b07069896fc315979970265bc4128287658a
Harshad Barapatre <harshadbarapatre@hotmail.com>
2021-05-10 01:14:45 +0530

    mr

--- Capstone/t2 Milestone-Report.R
+++ Capstone/t2 Milestone-Report.R
@@ -29,20 +29,32 @@ suppressMessages(suppressWarnings(library(gridExtra)))
 # close(cnxn)
 # 
 # rm(cnxn)
-
-#File Summary
-summary <- sapply(list(blogs_lines,news_lines,twitter_lines),function(x) summary(stri_count_words(x))[c('Min.','Mean','Max.')])
-rownames(summary) <- c('Min','Mean','Max')
-stats <- data.frame(
-  FileName=c("en_US.blogs","en_US.news","en_US.twitter"),      
-  t(rbind(sapply(list(blogs_lines,news_lines,twitter_lines),stri_stats_general)[c('Lines','Chars'),],  Words=sapply(list(blogs_lines,news_lines,twitter_lines),stri_stats_latex)['Words',], summary)))
-head(stats)
-
-# Get file sizes
-blogs_size <- file.info(blogs_file)$size / 1024 ^ 2
-news_size <- file.info(news_file)$size / 1024 ^ 2
-twitter_size <- file.info(twitter_file)$size / 1024 ^ 2
+# 
+# #File Summary
+# summary <- sapply(list(blogs_lines,news_lines,twitter_lines),function(x) summary(stri_count_words(x))[c('Min.','Mean','Max.')])
+# rownames(summary) <- c('Min','Mean','Max')
+# stats <- data.frame(
+#   FileName=c("en_US.blogs","en_US.news","en_US.twitter"),      
+#   t(rbind(sapply(list(blogs_lines,news_lines,twitter_lines),stri_stats_general)[c('Lines','Chars'),],  Words=sapply(list(blogs_lines,news_lines,twitter_lines),stri_stats_latex)['Words',], summary)))
+# head(stats)
+# 
+# # Get file sizes
+# blogs_size <- file.info(blogs_file)$size / 1024 ^ 2
+# news_size <- file.info(news_file)$size / 1024 ^ 2
+# twitter_size <- file.info(twitter_file)$size / 1024 ^ 2
 
 # Summary of dataset
 df<-data.frame(Doc = c("blogs", "news", "twitter"), Size.MB = c(blogs_size, news_size, twitter_size), Num.Lines = c(length(blogs_lines), length(news_lines), length(twitter_lines)), Num.Words=c(sum(nchar(blogs_lines)), sum(nchar(news_lines)), sum(nchar(twitter_lines))))
 df
+
+# Sampling data from the corpus
+sampleBlogs <- blogs_lines[sample(1:length(blogs_lines), 0.001*length(blogs_lines), replace=FALSE)]
+sampleNews <- news_lines[sample(1:length(news_lines), 0.001*length(news_lines), replace=FALSE)]
+sampleTwitter <- twitter_lines[sample(1:length(twitter_lines), 0.001*length(twitter_lines), replace=FALSE)]
+
+# Cleaning sampled data
+sampleBlogs <- iconv(sampleBlogs, "UTF-8", "ASCII", sub="")
+sampleNews <- iconv(sampleNews, "UTF-8", "ASCII", sub="")
+sampleTwitter <- iconv(sampleTwitter, "UTF-8", "ASCII", sub="")
+data.sample <- c(sampleBlogs,sampleNews,sampleTwitter)
+
